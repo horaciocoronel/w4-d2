ad891230ec7d8cdb02a73382519a8e1d3708c838
horaciocoronel <arielc@gmail.com>
2017-10-03 16:03:30 -0400

    Finish exercise 03 - stretch goal

--- 03-first-web-app/server.rb
+++ 03-first-web-app/server.rb
@@ -1,5 +1,9 @@
 require 'sinatra'
 
+get '/' do
+  redirect to('/home')
+end
+
 get '/home' do
   erb :index
 end
@@ -8,6 +12,17 @@ get '/portfolio' do
   erb :gallery
 end
 
+get '/gallery' do
+  redirect to('/portfolio')
+end
+
 get '/about_me' do
+  @skills = ['git', 'HTML', 'CSS', 'Ruby']
+  @interests = ['cats', 'art', 'music', 'films', 'coffee']
   erb :about_me
+end
+
+get '/favourites' do
+  @fav_links = ['www.google.com', 'www.apple.com','www.microsoft.com']
+  erb :favourites
 end
